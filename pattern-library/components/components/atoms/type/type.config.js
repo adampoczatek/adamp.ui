aa64a1c95fd9ed6ebfa19681914c776b47ae92a3
Development <adam-dev@Adams-MacBook-Pro.local>
2018-01-29 00:06:39 +0000

    Working on 1.2.0: * Added  atom * Cleaned sketch file

---
+++ pattern-library/components/components/atoms/type/type.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  label: "Type",
+  name: "type",
+  default: "insignificant",
+  variants: [
+    {
+      name: "insignificant",
+      label: "Insignificant",
+      context: {
+        text: "Some text that isn't important",
+        variant: "insignificant",
+      }
+    },
+  ]
+};
--- pattern-library/components/components/organisms/footer/footer.config.js
+++ pattern-library/components/components/organisms/footer/footer.config.js
@@ -1,4 +1,4 @@
 module.exports = {
   name: 'footer',
-  label: 'footer',
+  label: 'Footer',
 };
